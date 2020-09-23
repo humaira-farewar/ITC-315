d417a5ea913217f428ed46ca6be1d83fcde8695e
zahra123764 <71792994+zahra123764@users.noreply.github.com>
2020-09-24 02:04:29 +0430

    summation4.java
    Added the power function aswell!

--- summation.java
+++ summation.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
  
 public class Exercise5 {
  
@@ -12,6 +13,7 @@ public class Exercise5 {
   int num2 = in.nextInt();
   
   System.out.println("Hi world!"); 
+ System.out.println(num1 +" ^ "+num2+" = "+ Math.pow(num1, num2));
   System.out.println(num1 + " x " + num2 + " = " + num1 * num2);
   System.out.println(num1 +" + "+num2+" = "+ (num1+num2));
   System.out.println(num1 +" / "+num2+" = "+ (num1/num2));
