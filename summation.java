8da2b923f5f3d8c09a468b8efe95069b0c4ba839
zahra123764 <71792994+zahra123764@users.noreply.github.com>
2020-09-24 02:46:00 +0430

    summation2-3.java
    adding three numbers

--- summation.java
+++ summation.java
@@ -10,11 +10,15 @@ public class Exercise5 {
    
   System.out.print("Input second number: ");
   int num2 = in.nextInt();
+  System.out.print("Input second number: ");
+  int num3 = in.nextInt();
   
-  System.out.println("Hi world!"); 
-  System.out.println(num1 + " x " + num2 + " = " + num1 * num2);
+  System.out.println("Hi world!");
+  
+  System.out.println(num1 + " x " + num2 +  " x " + num3 +  " = " + num1 * num2 *num3);
   System.out.println(num1 +" + "+num2+" = "+ (num1+num2));
   System.out.println(num1 +" / "+num2+" = "+ (num1/num2));
+  
   
   
                      
