6aa3b6fa8765a2aac5d267259b40548b268b79a6
zahra123764 <71792994+zahra123764@users.noreply.github.com>
2020-09-24 01:42:33 +0430

    summation2.java
    adding new operation

--- summation.java
+++ summation.java
@@ -12,6 +12,9 @@ public class Exercise5 {
   int num2 = in.nextInt();
    
   System.out.println(num1 + " x " + num2 + " = " + num1 * num2);
+  System.out.println(num1 +" + "+num2+" = "+ (num1+num2));
+  
+                     
  }
  
 }
