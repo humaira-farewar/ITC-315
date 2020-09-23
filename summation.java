e08c1e27d85e5f1a23bb5db84006ea1c8c9e801e
zahra123764 <71792994+zahra123764@users.noreply.github.com>
2020-09-24 02:54:51 +0430

    summation2-4.java

--- summation.java
+++ summation.java
@@ -15,7 +15,8 @@ public class Exercise5 {
   
   System.out.println("Hi world!");
   
-  System.out.println(num1 + " x " + num2 +  " x " + num3 +  " = " + num1 * num2 *num3);
+  //System.out.println(num1 + " x " + num2 +  " x " + num3 +  " = " + num1 * num2 *num3);
+  System.out.println(num1 + " x " + num2 +  " = " + num1 * num2);
   System.out.println(num1 +" + "+num2+" = "+ (num1+num2));
   System.out.println(num1 +" / "+num2+" = "+ (num1/num2));
   
