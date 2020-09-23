b6067b2ffc411248658be6080809fa639be81e88
zahra123764 <71792994+zahra123764@users.noreply.github.com>
2020-09-24 01:43:39 +0430

    summation3.java
    Adding division operation

--- summation.java
+++ summation.java
@@ -13,6 +13,7 @@ public class Exercise5 {
    
   System.out.println(num1 + " x " + num2 + " = " + num1 * num2);
   System.out.println(num1 +" + "+num2+" = "+ (num1+num2));
+  System.out.println(num1 +" / "+num2+" = "+ (num1/num2));
   
                      
  }
